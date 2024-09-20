0e6dba5ba842a2b1fc837d04669519349463ac46
pcbogota <pcbogota.com@gmail.com>
2024-09-19 21:29:04 -0500

    Updating comments

--- Get-OldMenu.ps1
+++ Get-OldMenu.ps1
@@ -1,4 +1,4 @@
-﻿#reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+﻿# reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 # Mensaje de bienvenida
 Write-Host 'Activando el antiguo menú contextual en Windows 11'
